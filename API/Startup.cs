9821d274ad85bf9200b5a758bf58a417270511e4
brumanu <67878766+brumanu@users.noreply.github.com>
2021-09-22 23:31:01 -0300

    Configuração inicial do Cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,14 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Configurar a politica de Cors
+            services.AddCors(
+                options =>
+                {
+                    options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin());
+                }
+            )
+
             //Configurar todas as injeções de dependência do seu projeto
             services.AddDbContext<DataContext>(
                 options => options.UseInMemoryDatabase("database")
@@ -49,6 +57,8 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
 
