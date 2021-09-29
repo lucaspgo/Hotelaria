488ae459925f34e5b676eb33f29da8708b096586
Lucas Pereira Gonçalves de Oliveira <lucaspgo@outlook.com>
2021-09-29 16:31:02 -0300

    Quarto Rota pra Editar

--- FrontEnd/src/app/components/views/quarto/listar-quarto/listar-quarto.component.ts
+++ FrontEnd/src/app/components/views/quarto/listar-quarto/listar-quarto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { Quarto } from "src/app/models/Quarto";
 import { QuartoService } from "src/app/services/quarto.service";
 
@@ -9,11 +10,17 @@ import { QuartoService } from "src/app/services/quarto.service";
 })
 export class ListarQuartoComponent implements OnInit {
   quartos: Quarto[] = [];
-  constructor(private service: QuartoService) {}
+  constructor(private service: QuartoService, private router: Router) {}
 
   ngOnInit(): void {
     this.service.list().subscribe((quartos) => {
       this.quartos = quartos;
     });
   }
+
+  onEdit(id: number): void {
+    this.router.navigate([`quarto/editar/${id}`]);
+  }
+
+  onDelete(quarto: Quarto): void {}
 }
